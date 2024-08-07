7d8af82a376699d066b14991ab4291a24e8e9751
한준범 <fullgukbap@hanjunbeom-ui-MacBookAir.local>
2024-08-07 16:42:33 +0900

    parser error 보이게 출력

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
@@ -9,13 +10,15 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := Parser{
-		l: l,
+		l:      l,
+		errors: []string{},
 	}
 
 	// curToken과 peekToken에 토큰 값이 담기게끔 설정
@@ -23,6 +26,15 @@ func New(l *lexer.Lexer) *Parser {
 	p.nextToken()
 
 	return &p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -47,6 +59,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
+	// 'let' x = 5;
 	case token.LET:
 		return p.parseLetStatement()
 	default:
@@ -57,6 +70,7 @@ func (p *Parser) parseStatement() ast.Statement {
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
+	// let 'x' = 5;
 	if !p.expectPeek(token.IDENT) {
 		return nil
 	}
@@ -66,11 +80,12 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		Value: p.curToken.Literal,
 	}
 
+	// let x '=' 5;
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
 
-	// 세미콜론 만날 떄 까지 반복 (왼래는 Value에 값을 넣어줘야 함)
+	// 아직 expression parsing 방법을 몰라서 아래 코드로 대체
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
@@ -86,11 +101,13 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 	return p.peekToken.Type == t
 }
 
+// expectPeek 함수는 p.peekToken과 t가 참과 함께 curToken과 peekToken을 다음으로 이동시킨다.
 func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
