67e81ea6a86ec8c42ad3e9bb4114716c93dcb4ec
한준범 <fullgukbap@hanjunbeom-ui-MacBookAir.local>
2024-08-07 22:20:52 +0900

    개발에 용이하게 Node interface에 메소드 추가

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,15 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 // Ast를 구성하는 모든 노드는 Node interface를 구현해야 한다.
 type Node interface {
 	// TokenLiteral은 토큰에 대응되는 리터럴을 반환합니다.
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -33,6 +37,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token // 이건 어디에 사용되나?
 	// Name 변수는 바인딩 식별자 값을 저장합니다.
@@ -41,8 +55,23 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (l *LetStatement) statementNode()       {}
-func (l *LetStatement) TokenLiteral() string { return l.Token.Literal }
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token
@@ -52,6 +81,7 @@ type Identifier struct {
 // 식별자가 값을 생성하기 때문에 expression node이다.
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type ReturnStatement struct {
 	Token       token.Token
@@ -60,3 +90,23 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // 표현식의 첫 번째 토큰
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
